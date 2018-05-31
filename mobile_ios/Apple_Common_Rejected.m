0f7399791ebe7939599a8776f0fbad5dbcec02ee
Le Minh Tuan <leminhtuan2015@gmail.com>
2018-05-31 09:11:53 +0700

    Create Apple_Common_Rejected.m

---
+++ mobile_ios/Apple_Common_Rejected.m
@@ -0,0 +1,10 @@
+* Minimum function
+* Asking for access (location, gallery, camera) not clearly
+* Crash on any function
+* UI not good
+
+* Check Advertising Identifier
+
+```
+fgrep -R advertisingIdentifier ./
+```
