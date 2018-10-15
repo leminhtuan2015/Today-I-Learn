230f0cf274afe414b6c2e6e765776b39868bb0ee
Le Minh Tuan <leminhtuan2015@gmail.com>
2018-10-15 09:56:39 +0700

    Update keystone_custome.js

--- backend_javascript/keystone_js/keystone_custome.js
+++ backend_javascript/keystone_js/keystone_custome.js
@@ -1,1 +1,16 @@
 
+### Keep original upload file name
+
+* PATH: node_modules/keystone-storage-namefunctions/index.js
+
+```js
+// Generate a random filename
+exports.randomFilename = function (file, i, callback) {
+	// crypto.randomBytes(16, function (err, data) {
+	// 	if (err) return callback(err);
+	// 	return callback(null, filenameFromBuffer(data, file.extension));
+	// });
+
+	return callback(null, file.originalname);
+};
+```
